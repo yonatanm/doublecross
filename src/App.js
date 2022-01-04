56a4e647dd3e5beecdf4fae63495c2e97244e6b5
Yonatan Maman <yonatan@outbrain.com>
2022-01-04 19:25:49 +0200

    menue

--- src/App.js
+++ src/App.js
@@ -1,47 +1,76 @@
 import "./App.scss";
-
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
 
 import Crossword from "./components/Crossword";
 import AllCrosswords from "./components/AllCrosswords";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import { useNavigate } from "react-router-dom";
+import SearchAppBar from "./components/Menu";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+// import { create } from 'jss';
+// import rtl from 'jss-rtl';
+// import { StylesProvider, jssPreset } from '@mui/styles';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  direction: "rtl",
+});
+
+// const jss = create({
+//   plugins: [...jssPreset().plugins, rtl()],
+// });
+
+// Create rtl cache
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [rtlPlugin],
+});
+
+function RTL(props) {
+  return <CacheProvider value={cacheRtl}>{props.children}</CacheProvider>;
+  // return <></>
+}
+
+// function RTL(props) {
+//   return <StylesProvider jss={jss}>{props.children}</StylesProvider>;
+// }
 
 function App() {
-  
-
   return (
-    <Router>
-      <div dir='rtl'>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/crossword">New</Link>
-            </li>            
-            <li>
-              <Link to="/crosswords">all</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
+    <RTL>
+      {/* <ThemeProvider theme={theme}> */}
+      <Router>
+        <SearchAppBar> </SearchAppBar>
+        <div>
+          {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
-        <Routes>
-          <Route path="/crossword" element={<Crossword />} />
-          <Route exact path="/crosswords" element={<AllCrosswords />} />
-          <Route path="/crosswords/:id" element={<Crossword />} />
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            <Route path="/crossword" element={<Crossword />} />
+            <Route exact path="/crosswords" element={<AllCrosswords />} />
+            <Route path="/crosswords/:id" element={<Crossword />} />
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+      {/* </ThemeProvider> */}
+    </RTL>
   );
 }
 
 function Home() {
+  const navigate = useNavigate();
+  const gotoNew = () => {
+    navigate(`/crossword`);
+  };
   return (
     <div>
+      <Fab color="primary" aria-label="add" onClick={gotoNew}>
+        <AddIcon />
+      </Fab>
+
       <h1>DoubleCrossברוך הבא ל</h1>
       {/* <Outlet/> */}
     </div>
---
+++ src/components/Menu.js
@@ -0,0 +1,121 @@
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useNavigate } from "react-router-dom";
+
+import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+  const navigate = useNavigate();
+
+  const onClick = (p, dest) => {
+    p.close();
+    navigate(`${dest}`);
+  };
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <PopupState variant="popover" popupId="demo-popup-menu">
+            {(popupState) => (
+              <React.Fragment>
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  aria-label="open drawer"
+                  sx={{ mr: 2 }}
+                  variant="contained"
+                  {...bindTrigger(popupState)}
+                >
+                  <MenuIcon />
+                </IconButton>
+
+                <Menu {...bindMenu(popupState)}>
+                  <MenuItem onClick={() => onClick(popupState, '/')}>
+                    עמוד הבית
+                  </MenuItem>
+                  <MenuItem onClick={() => onClick(popupState, '/crosswords')}>
+                    התשבצים שלי
+                  </MenuItem>
+                  <MenuItem onClick={() => onClick(popupState, '/crossword')}>
+                    תשבץ חדש
+                  </MenuItem>
+                </Menu>
+              </React.Fragment>
+            )}
+          </PopupState>
+
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            MUI
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
